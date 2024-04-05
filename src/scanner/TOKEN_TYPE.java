69bf1f0eae499c122a431e91ba1599d932a17b41
Ágata Meireles Carvalho <84482172+agatameireles11@users.noreply.github.com>
2024-04-04 21:34:47 -0300

    Add file TOKEN_TYPE.java

---
+++ src/scanner/TOKEN_TYPE.java
@@ -0,0 +1,33 @@
+//TIPOS DE TOKEN
+public enum TOKEN_TYPE {
+    INT,
+    FLOAT,
+    BOOLEAN,
+    NULL,
+    ID,
+    TYPE_NAME,
+    CHAR, 
+    OPEN_PARENTHESIS, //( 
+    CLOSE_PARENTHESIS, //)
+    OPEN_BRACKET, //[
+    CLOSE_BRACKET, //]
+    OPEN_CURLY_BRACE, //}
+    CLOSE_CURLY_BRACE, //}
+    GREATER_THAN, //>
+    LESS_THAN, //<
+    SEMICOLON, //;
+    DOUBLE_COLON, //::
+    COLON, //:
+    DOT, //.
+    COMMA, //,
+    COMPARISON, //==
+    EQUAL, //=
+    NOT_EQUAL, //!=
+    PLUS, //+
+    MINUS, //-
+    TIMES, // *
+    DIVISION, // /
+    MODULUS, //%
+    AND, //&&
+    NOT, //!   
+}

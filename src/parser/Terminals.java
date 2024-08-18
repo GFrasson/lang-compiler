825084ccf5de8301abeaad2cbbe8ebd0248969c2
GFrasson <bielfc27@gmail.com>
2024-08-18 16:41:01 -0300

    Fixing Less than shift-reduce conflict

---
+++ src/parser/Terminals.java
@@ -0,0 +1,47 @@
+package parser;
+
+/**
+ * This class lists terminals used by the
+ * grammar specified in "lang.grammar".
+ */
+public class Terminals {
+	static public final short EOF = 0;
+	static public final short ID = 1;
+	static public final short MINUS = 2;
+	static public final short OPEN_PARENTHESIS = 3;
+	static public final short NOT = 4;
+	static public final short BOOLEAN = 5;
+	static public final short NULL = 6;
+	static public final short INT = 7;
+	static public final short FLOAT = 8;
+	static public final short CHAR = 9;
+	static public final short NEW = 10;
+	static public final short CLOSE_PARENTHESIS = 11;
+	static public final short TIMES = 12;
+	static public final short DIVISION = 13;
+	static public final short MODULUS = 14;
+	static public final short OPEN_CURLY_BRACE = 15;
+	static public final short AND = 16;
+	static public final short PLUS = 17;
+	static public final short OPEN_BRACKET = 18;
+	static public final short SEMICOLON = 19;
+	static public final short COMMA = 20;
+	static public final short CLOSE_CURLY_BRACE = 21;
+	static public final short TYPE_NAME = 22;
+	static public final short IF = 23;
+	static public final short ITERATE = 24;
+	static public final short READ = 25;
+	static public final short PRINT = 26;
+	static public final short RETURN = 27;
+	static public final short CLOSE_BRACKET = 28;
+	static public final short DOT = 29;
+	static public final short LESS_THAN = 30;
+	static public final short DOUBLE_COLON = 31;
+	static public final short GREATER_THAN = 32;
+	static public final short DATA = 33;
+	static public final short COMPARISON = 34;
+	static public final short NOT_EQUAL = 35;
+	static public final short EQUAL = 36;
+	static public final short COLON = 37;
+	static public final short ELSE = 38;
+}

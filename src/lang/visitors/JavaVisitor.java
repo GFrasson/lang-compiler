a6dd947b5fe0709a03d965a820150b9d89d7d824
Agata <agata.meireles@estudante.ufjf.br>
2024-09-29 09:16:15 -0300

    Add JavaVisitor. java and ST-4.3.1.jar

---
+++ src/lang/visitors/JavaVisitor.java
@@ -0,0 +1,19 @@
+/*
+* Equipe:
+* ÁGATA MEIRELES CARVALHO - 202065001C
+* GABRIEL FRASSON COSTA - 202035001
+*/
+
+package lang.visitors;
+
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
+import java.util.List;
+import java.util.ArrayList;
+
+public class JavaVisitor {
+
+
+    
+}
